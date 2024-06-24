2056751c79d32fc65e7384ac22161bb8822f9841
BenchPress200 <jms0538@naver.com>
2024-06-24 20:22:54 +0900

    test

--- public/javascript/global.js
+++ public/javascript/global.js
@@ -3,3 +3,5 @@ export const BACKEND_PORT = 8080;
 
 export const FRONTEND_IP_PORT = `http://localhost:${FRONTEND_PORT}`;
 export const BACKEND_IP_PORT = `http://localhost:${BACKEND_PORT}`;
+
+// 시크릿에 담는코드 ㄱㄱ
