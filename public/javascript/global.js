460ef570f9d1e52d64c3fdae2d8579d8ed0af4b9
BenchPress200 <jms0538@naver.com>
2024-07-01 02:26:51 +0900

    refactor: modify endpoint

--- public/javascript/global.js
+++ public/javascript/global.js
@@ -1,12 +1,11 @@
 export const FRONTEND_PORT = 3000;
 export const BACKEND_PORT = 8080;
 
-export const FRONTEND_IP_PORT = `http://localhost:${FRONTEND_PORT}`;
-export const BACKEND_IP_PORT = `http://localhost:${BACKEND_PORT}`;
+export const FRONTEND_IP_PORT = `http://localhost:${FRONTEND_PORT}/api/v1`;
+export const BACKEND_IP_PORT = `http://localhost:${BACKEND_PORT}/api/v1`;
 
 // // 시크릿에 담는코드 ㄱㄱ
 // export const FRONTEND_PORT = 3000;
 // export const BACKEND_PORT = 8080;
 
-// export const FRONTEND_IP_PORT = \`http://localhost:\${FRONTEND_PORT}\`;
-// export const BACKEND_IP_PORT = \`http://localhost:\${BACKEND_PORT}\`;
+// export const BACKEND_IP_PORT = \`http://ec2-15-165-77-151.ap-northeast-2.compute.amazonaws.com/api/v1\`;
